665faec3c07a730c629c31bb3a73b3eb73df9cc0
rjcarter08 <71043468+rjcarter08@users.noreply.github.com>
2020-12-06 15:25:21 -0600

    Add files via upload

---
+++ MyShell.py
@@ -0,0 +1,24 @@
+import os 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","learning_log.settings")
+
+import django
+django.setup() 
+
+from learning_logs.models import Topic 
+
+topics = Topic.objects.all() 
+
+for topic in topics: 
+    print("Topic ID:", topic.id, "Topic:", topic.text)
+
+
+t = Topic.objects.get(id=1) 
+print(t.text)
+print(t.date_added)
+
+
+entries = t.entry_set.all() 
+
+for entry in entries: 
+    print(entry) 
+    
